a557c3a5e6e196747d328b9344c14da77fc56742
irfanzahoor <irfanzahoor250@gmail.com>
2024-07-04 13:01:25 +0500

    fix icons

--- technical_creed_v3/public/js/script.js
+++ technical_creed_v3/public/js/script.js
@@ -3,32 +3,27 @@
 
     var $window = $(window);
 
-     // :: Sticky Active Code
-    $window.on("scroll", function(){
-        if
-      ($(document).scrollTop() > 86){
-          $("#banner").addClass("shrink");
-        }
-        else
-        {
+    // :: Sticky Active Code
+    $window.on("scroll", function() {
+        if ($(document).scrollTop() > 86) {
+            $("#banner").addClass("shrink");
+        } else {
             $("#banner").removeClass("shrink");
         }
     });
 
-
     // :: Carousel Active Code
     if ($.fn.owlCarousel) {
-
         $(".client_slides").owlCarousel({
             responsive: {
-            0: {
-                items: 1
-            },
-            991: {
-                    items: 2
-                },
-            767:{
+                0: {
                     items: 1
+                },
+                767: {
+                    items: 1
+                },
+                991: {
+                    items: 2
                 }
             },
             loop: true,
@@ -38,7 +33,7 @@
         });
 
         var dot = $('.client_slides .owl-dot');
-        dot.each(function () {
+        dot.each(function() {
             var index = $(this).index() + 1;
             if (index < 10) {
                 $(this).html('0').append(index);
@@ -54,12 +49,12 @@
             type: 'iframe'
         });
         $('.open-popup-link').magnificPopup({
-          type:'inline',
-          midClick: true 
+            type: 'inline',
+            midClick: true
         });
         $('.open-signup-link').magnificPopup({
-          type:'inline',
-          midClick: true 
+            type: 'inline',
+            midClick: true
         });
         $('.gallery_img').magnificPopup({
             type: 'image',
@@ -71,11 +66,6 @@
             preloader: true
         });
     }
-
-
-
-    
-
 
     // :: onePageNav Active Code
     if ($.fn.onePageNav) {
@@ -94,20 +84,18 @@
         });
     }
 
-
-
     // :: Wow Active Code
     if ($window.width() > 767) {
         new WOW().init();
     }
 
     // :: Accordian Active Code
-    (function () {
+    (function() {
         var dd = $('dd');
         dd.filter(':nth-child(n+3)').hide();
-        $('dl').on('click', 'dt', function () {
+        $('dl').on('click', 'dt', function() {
             $(this).next().slideDown(500).siblings('dd').slideUp(500);
-        })
+        });
     })();
 
     // :: niceScroll Active Code
@@ -115,8 +103,34 @@
         $(".timelineBody").niceScroll();
     }
 
-    //$('body').bind('cut copy paste',function(e){e.preventDefault()});$("body").on("contextmenu",function(e){return!1});document.onkeydown=function(e){if(e.ctrlKey&&(e.keyCode===67||e.keyCode===86||e.keyCode===85||e.keyCode===117)){alert('This is not allowed');return!1}else{return!0}};$(document).keydown(function(event){if(event.keyCode==123){return!1}
-    //else if((event.ctrlKey&&event.shiftKey&&event.keyCode==73)||(event.ctrlKey&&event.shiftKey&&event.keyCode==74)){return!1}});var isCtrl=!1;document.onkeyup=function(a){17==a.which&&(isCtrl=!1)},document.onkeydown=function(a){if(17==a.which&&(isCtrl=!0),85==a.which||67==a.which&&1==isCtrl)return!1};
-
-
+    // The following code is commented out. Uncomment and fix only if required.
+    /*
+    $('body').bind('cut copy paste', function(e) { e.preventDefault(); });
+    $("body").on("contextmenu", function(e) { return false; });
+    document.onkeydown = function(e) {
+        if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {
+            alert('This is not allowed');
+            return false;
+        } else {
+            return true;
+        }
+    };
+    $(document).keydown(function(event) {
+        if (event.keyCode == 123) {
+            return false;
+        } else if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
+            return false;
+        }
+    });
+    var isCtrl = false;
+    document.onkeyup = function(e) {
+        if (e.which == 17) isCtrl = false;
+    };
+    document.onkeydown = function(e) {
+        if (e.which == 17) isCtrl = true;
+        if ((e.which == 85 || e.which == 67) && isCtrl == true) {
+            return false;
+        }
+    };
+    */
 })(jQuery);
