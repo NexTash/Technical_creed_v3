4ca3c3ae443b8d50c0885eef582266e31de5fb6c
irfanzahoor <irfanzahoor250@gmail.com>
2024-07-06 19:08:29 +0500

    assits fix

--- technical_creed_v3/www/faq.py
+++ technical_creed_v3/www/faq.py
@@ -2,9 +2,7 @@ import frappe
 
 def get_context(context):
     docs = frappe.get_all("FAQ", ["*"] , order_by="modified asc")
-    items = frappe.get_all("Team", ["*"] , order_by="modified asc")
     context.update({
         "docs":docs,
-        "items":items,
     })
     
--- technical_creed_v3/www/pricing.py
+++ technical_creed_v3/www/pricing.py
@@ -2,9 +2,9 @@ import frappe
 
 def get_context(context):
     docs = frappe.get_all("Pricing", ["*"] , order_by="modified asc")
-    items = frappe.get_all("Team", ["*"] , order_by="modified asc")
+    # items = frappe.get_all("Team", ["*"] , order_by="modified asc")
 
     context.update({
         "docs":docs,
-        "items":items,
+        # "items":items,
     })
